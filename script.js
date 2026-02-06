b7d760fc2d1087907520312285e76e34f2f5098c
Yogesh Jumbad <jumbadyogesh99@gmail.com>
2026-02-06 12:17:58 +0530

    added login and register forms

--- script.js
+++ script.js
@@ -1,5 +1,45 @@
-// Smooth scroll is handled by CSS
-console.log('🌍 GreenEarth');
+// Login popup
+const loginBtn = document.getElementById("desktop-login");
+const overlay = document.getElementById("overlay");
+const closeBtn = document.getElementById("closeBtn");
+
+// Open popup
+loginBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    overlay.style.display = "flex";
+}); 
+
+// Close popup (X button)
+closeBtn.addEventListener("click", () => {
+    overlay.style.display = "none";
+});
+
+// Handle form submission
+document.getElementById("loginForm").addEventListener("submit", (e) => {
+    e.preventDefault();
+    alert("Login submitted!");
+    overlay.style.display = "none";
+});
 
 
+// register popup
+  const registerBtn = document.getElementById("registerBtn");
+  const regOverlay = document.getElementById("regOverlay");
+  const closeReg = document.getElementById("closeReg");
 
+  // Open popup
+  registerBtn.addEventListener("click", () => {
+    regOverlay.style.display = "flex";
+  });
+
+  // Close popup
+  closeReg.addEventListener("click", () => {
+    regOverlay.style.display = "none";
+  });
+
+  // Close when clicking outside
+  regOverlay.addEventListener("click", (e) => {
+    if (e.target === regOverlay) {
+      regOverlay.style.display = "none";
+    }
+  });
